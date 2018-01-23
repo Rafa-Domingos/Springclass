b956b5b40e9050249eaf8311cc3332921932ed1c
Rafael Santos <rafinha_2003@hotmail.com>
2018-01-23 13:13:27 -0200

    Initial structure

---
+++ src/main/java/com/rafael/springclass/SpringclassApplication.java
@@ -0,0 +1,12 @@
+package com.rafael.springclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringclassApplication.class, args);
+	}
+}
