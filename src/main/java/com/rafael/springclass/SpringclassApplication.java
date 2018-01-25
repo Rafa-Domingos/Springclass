aa0b7f3262b55f66e5ddae793816aa815ed24b4e
Rafael Santos <rafinha_2003@hotmail.com>
2018-01-25 20:24:41 -0200

    Persisting categories

--- src/main/java/com/rafael/springclass/SpringclassApplication.java
+++ src/main/java/com/rafael/springclass/SpringclassApplication.java
@@ -1,12 +1,27 @@
 package com.rafael.springclass;
 
+import com.rafael.springclass.domain.Category;
+import com.rafael.springclass.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringclassApplication {
+public class SpringclassApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringclassApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.categoryRepository.save(Arrays.asList(new Category(null, "Computing"),
+				new Category(null, "Office")));
 	}
 }
