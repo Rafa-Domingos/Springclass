8acfc90d86e32a06fd6d32eac8967875bed78794
Rafael Santos <rafinha_2003@hotmail.com>
2018-01-25 12:59:37 -0200

    Addition of h2 database and automatic database creation

--- src/main/java/com/rafael/springclass/domain/Category.java
+++ src/main/java/com/rafael/springclass/domain/Category.java
@@ -4,8 +4,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 @EqualsAndHashCode(exclude = {"name"})
 public class Category implements Serializable {
 
@@ -19,6 +24,8 @@ public class Category implements Serializable {
      */
     @Getter
     @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     /**
